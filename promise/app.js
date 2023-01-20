72217a68fb8bf00138d2cbc3b7d316af81bb2f80
aim <ahmadhakim404@gmail.com>
2023-01-21 02:43:48 +0700

    still callback hell in promise

--- promise/app.js
+++ promise/app.js
@@ -15,7 +15,7 @@ const requestPromise = (url) => {
 	return new Promise((resolve, reject) => {
 		const delay = Math.floor(Math.random() * 4500) + 500;
 		setTimeout(() => {
-			if (delay > 4000) {
+			if (delay > 3000) {
 				reject('Error: Connection Timeout');
 			} else {
 				resolve(`Success: ${url} (${delay}ms)`);
@@ -24,48 +24,84 @@ const requestPromise = (url) => {
 	});
 };
 
-requestCallback(
-	'movie.com',
-	function (response) {
+requestPromise('movie.com')
+	.then((response) => {
 		console.log('success', response);
-		requestCallback(
-			'movie.com',
-			function (response) {
+		requestPromise('movie.com')
+			.then((response) => {
 				console.log('success', response);
-				requestCallback(
-					'movie.com',
-					function (response) {
+				requestPromise('movie.com')
+					.then((response) => {
 						console.log('success', response);
-						requestCallback(
-							'movie.com',
-							function (response) {
+						requestPromise('movie.com')
+							.then((response) => {
 								console.log('success', response);
-								requestCallback(
-									'movie.com',
-									function (response) {
+								requestPromise('movie.com')
+									.then((response) => {
 										console.log('success', response);
-									},
-									function (error) {
+									})
+									.catch((error) => {
 										console.log('error', error);
-									}
-								);
-							},
-							function (error) {
+									});
+							})
+							.catch((error) => {
 								console.log('error', error);
-							}
-						);
-					},
-					function (error) {
+							});
+					})
+					.catch((error) => {
 						console.log('error', error);
-					}
-				);
-			},
-			function (error) {
+					});
+			})
+			.catch((error) => {
 				console.log('error', error);
-			}
-		);
-	},
-	function (error) {
+			});
+	})
+	.catch((error) => {
 		console.log('error', error);
-	}
-);
+	});
+
+// requestCallback(
+// 	'movie.com',
+// 	function (response) {
+// 		console.log('success', response);
+// 		requestCallback(
+// 			'movie.com',
+// 			function (response) {
+// 				console.log('success', response);
+// 				requestCallback(
+// 					'movie.com',
+// 					function (response) {
+// 						console.log('success', response);
+// 						requestCallback(
+// 							'movie.com',
+// 							function (response) {
+// 								console.log('success', response);
+// 								requestCallback(
+// 									'movie.com',
+// 									function (response) {
+// 										console.log('success', response);
+// 									},
+// 									function (error) {
+// 										console.log('error', error);
+// 									}
+// 								);
+// 							},
+// 							function (error) {
+// 								console.log('error', error);
+// 							}
+// 						);
+// 					},
+// 					function (error) {
+// 						console.log('error', error);
+// 					}
+// 				);
+// 			},
+// 			function (error) {
+// 				console.log('error', error);
+// 			}
+// 		);
+// 	},
+// 	function (error) {
+// 		console.log('error', error);
+// 	}
+// );
