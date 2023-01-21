3b28c0092c227b7bdb9c6ad7a92ee71792b1b170
aim <ahmadhakim404@gmail.com>
2023-01-21 12:07:53 +0700

    contoh handling error async await

--- promise/app.js
+++ promise/app.js
@@ -15,7 +15,7 @@ const requestPromise = (url) => {
 	return new Promise((resolve, reject) => {
 		const delay = Math.floor(Math.random() * 4500) + 500;
 		setTimeout(() => {
-			if (delay > 3000) {
+			if (delay > 2000) {
 				reject('Error: Connection Timeout');
 			} else {
 				resolve(`Success: ${url} (${delay}ms)`);
@@ -24,23 +24,32 @@ const requestPromise = (url) => {
 	});
 };
 
-requestPromise('movie.com')
-	.then((result) => {
-		console.log('page 1');
+async function requestHandler() {
+	try {
+		let result = await requestPromise('movie.com');
 		console.log(result);
-		return requestPromise('movie.com');
-	})
-	.then(() => {
-		console.log('page 2');
-		return requestPromise('movie.com');
-	})
-	.then(() => {
-		console.log('page 3');
-		return requestPromise('movie.com');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+	} catch (error) {
+		console.log('Pesan Error', error);
+	}
+}
+
+// requestPromise('movie.com')
+// 	.then((result) => {
+// 		console.log('page 1');
+// 		console.log(result);
+// 		return requestPromise('movie.com');
+// 	})
+// 	.then(() => {
+// 		console.log('page 2');
+// 		return requestPromise('movie.com');
+// 	})
+// 	.then(() => {
+// 		console.log('page 3');
+// 		return requestPromise('movie.com');
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
 
 // requestPromise('movie.com')
 // 	.then((response) => {
