0aea73d0ef0cccea29782de1e08de3624cddb17d
aim <ahmadhakim404@gmail.com>
2023-01-21 11:58:26 +0700

    change color dengan await

--- backgroundchanger/app.js
+++ backgroundchanger/app.js
@@ -7,10 +7,28 @@ const delayedColorChange = (color, delay) => {
 	});
 };
 
-delayedColorChange('red', 1000)
-	.then(() => delayedColorChange('yellow', 1000))
-	.then(() => delayedColorChange('green', 1000))
-	.then(() => delayedColorChange('blue', 1000))
-	.then(() => delayedColorChange('purple', 1000))
-	.then(() => delayedColorChange('orange', 1000))
-	.then(() => delayedColorChange('black', 1000));
+// delayedColorChange('red', 1000)
+// 	.then(() => delayedColorChange('yellow', 1000))
+// 	.then(() => delayedColorChange('green', 1000))
+// 	.then(() => delayedColorChange('blue', 1000))
+// 	.then(() => delayedColorChange('purple', 1000))
+// 	.then(() => delayedColorChange('orange', 1000))
+// 	.then(() => delayedColorChange('black', 1000));
+
+async function changeColor() {
+	await delayedColorChange('red', 1000);
+	await delayedColorChange('yellow', 1000);
+	await delayedColorChange('green', 1000);
+	await delayedColorChange('blue', 1000);
+	await delayedColorChange('purple', 1000);
+	await delayedColorChange('orange', 1000);
+	await delayedColorChange('black', 1000);
+	return 'All Done!';
+}
+
+async function printRainbow() {
+	await changeColor();
+	console.log('All Done! Dari printRainbow');
+}
+
+printRainbow();
