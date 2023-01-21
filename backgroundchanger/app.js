c87e0f7888a83987f83fd97419cbe84d028a059e
aim <ahmadhakim404@gmail.com>
2023-01-21 11:26:26 +0700

    add color changer with promise

---
+++ backgroundchanger/app.js
@@ -0,0 +1,16 @@
+const delayedColorChange = (color, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			document.body.style.backgroundColor = color;
+			resolve();
+		}, delay);
+	});
+};
+
+delayedColorChange('red', 1000)
+	.then(() => delayedColorChange('yellow', 1000))
+	.then(() => delayedColorChange('green', 1000))
+	.then(() => delayedColorChange('blue', 1000))
+	.then(() => delayedColorChange('purple', 1000))
+	.then(() => delayedColorChange('orange', 1000))
+	.then(() => delayedColorChange('black', 1000));
